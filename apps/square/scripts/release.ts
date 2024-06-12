cc9f9f8acfcb9cbc4a16d9f9870f4d4e00b6a8a0
Marvin Stürcke <marvin@mstuercke.dev>
2024-06-13 01:36:22 +0200

    Export cjs, esm and ts

--- apps/square/scripts/release.ts
+++ apps/square/scripts/release.ts
@@ -1,9 +1,7 @@
 import {releaseNpmPackage} from '@mstuercke/bun-utils'
 import packageJson from '../package.json'
 
-export default (async () => {
-  await releaseNpmPackage({
-    packageJson,
-    npmAuthToken: Bun.env.NPM_TOKEN as string,
-  })
-})()
+await releaseNpmPackage({
+  packageJson,
+  npmAuthToken: Bun.env.NPM_TOKEN as string,
+})
