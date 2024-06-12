17b71225025d1be81b4b87f5cf2541cf91d018bf
Marvin Stürcke <marvin@mstuercke.dev>
2024-06-13 01:36:22 +0200

    Export cjs, esm and ts

--- apps/core/scripts/build.ts
+++ apps/core/scripts/build.ts
@@ -1,8 +1,12 @@
 import * as path from 'path'
 import {buildNpmPackage, type TsConfigFilename} from '@mstuercke/bun-utils'
 
-export default (async () => {
-  await buildNpmPackage({
-    tsconfigFileName: path.resolve(__dirname, '../tsconfig.build.json') as TsConfigFilename,
-  })
-})()
+await buildNpmPackage({
+  tsconfigFileName: path.resolve(__dirname, '../tsconfig.build.cjs.json') as TsConfigFilename,
+  outDir: 'dist/cjs',
+})
+
+await buildNpmPackage({
+  tsconfigFileName: path.resolve(__dirname, '../tsconfig.build.esm.json') as TsConfigFilename,
+  outDir: 'dist/esm',
+})
