b08b24c4c2a857985cf8ffe141fe24def1830934
Marvin Stürcke <marvin@mstuercke.dev>
2024-06-13 12:57:58 +0200

    Remove unused type

--- apps/hexagon/src/HexagonDirection.ts
+++
@@ -1,4 +0,0 @@
-import type {FlatTopDirection} from './flatTop/FlatTopDirection'
-import type {PointyTopDirection} from './pointyTop/PointyTopDirection'
-
-export type HexagonDirection = FlatTopDirection | PointyTopDirection
