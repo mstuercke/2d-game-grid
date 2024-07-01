d0456e576ddc41c3b94ff232709dc30c4cf7896c
Marvin Stürcke <marvin@mstuercke.dev>
2024-07-01 23:04:46 +0200

    Use correct directions for test

--- apps/hexagon/src/flatTop/FlatTopHexagonCorners.unit.test.ts
+++ apps/hexagon/src/flatTop/FlatTopHexagonCorners.unit.test.ts
@@ -1,10 +1,10 @@
 import {FlatTopHexagonCorners} from './FlatTopHexagonCorners'
-import {DIAGONAL_DIRECTIONS} from '@2d-game-grid/core'
 import {FlatTopHexagonGrid} from './FlatTopHexagonGrid'
 import {preInitializedGridOptionsFixture} from './FlatTopHexagonGrid.fixture'
+import {FLAT_TOP_HEXAGON_CORNER_DIRECTIONS} from './FlatTopHexagonNeighborDirection'
 
 describe(FlatTopHexagonCorners.name, () => {
-  it.each(DIAGONAL_DIRECTIONS)('should have 3 neighbors for corner %s', (cornerDirection) => {
+  it.each(FLAT_TOP_HEXAGON_CORNER_DIRECTIONS)('should have 2 neighbors for corner %s', (cornerDirection) => {
     const grid = new FlatTopHexagonGrid(preInitializedGridOptionsFixture)
     const cell = grid.getCell({row: 1, col: 1})
 
