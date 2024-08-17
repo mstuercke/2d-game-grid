97b4308750a33a766f2187e22a2c990ce77e1bc8
Marvin Stürcke <marvin@mstuercke.dev>
2024-08-17 16:24:12 +0200

    Update @mstuercke/bun-utils imports

--- apps/hexagon/scripts/build.ts
+++ apps/hexagon/scripts/build.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
-import {buildNpmPackage, type TsConfigFilename} from '@mstuercke/bun-utils'
+import {buildNpmPackage} from '@mstuercke/bun-utils/npmPackage'
+import type {TsConfigFilename} from '@mstuercke/bun-utils/types'
 
 await buildNpmPackage({
   tsconfigFileName: path.resolve(__dirname, '../tsconfig.build.cjs.json') as TsConfigFilename,
