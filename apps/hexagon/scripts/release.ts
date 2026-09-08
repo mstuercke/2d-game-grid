c48f9fe222b014e7efee2d5c935772b31655e64e
Marvin Stürcke <marvin@mstuercke.dev>
2026-09-08 12:32:50 +0200

    ci: release with semantic-release and OIDC

--- apps/hexagon/scripts/release.ts
+++
@@ -1,7 +0,0 @@
-import {releaseNpmPackage} from '@mstuercke/node-utils/npmPackage'
-import packageJson from '../package.json' with {type: 'json'}
-
-await releaseNpmPackage({
-  packageJson,
-  npmAuthToken: process.env.NPM_TOKEN as string,
-})
